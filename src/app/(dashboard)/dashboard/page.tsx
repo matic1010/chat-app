c9841212bd4476b48569460f43e4c3a2f366856c
Nils Matic <matic1909@gmail.com>
2023-05-22 21:57:25 +0200

    added friend request functionality and middleware to protect routes

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,10 +1,5 @@
-import { authOptions } from "@/lib/auth";
-import { getServerSession } from "next-auth";
-
 const Dashboard = async ({}) => {
-  const session = await getServerSession(authOptions);
-
-  return <pre>{JSON.stringify(session)}</pre>;
+  return <pre>Dashboard</pre>;
 };
 
 export default Dashboard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+"use client";
+import { signOut } from "next-auth/react";
 import Button from "../components/ui/Button";
 
 export default async function Home() {
   return (
     <div className="text-red-500">
-      <Button>Hello</Button>
+      <Button onClick={() => signOut()}>Sign Out</Button>
     </div>
   );
 }
